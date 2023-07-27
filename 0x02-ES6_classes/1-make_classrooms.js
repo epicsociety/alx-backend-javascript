a095aee9e72cfdc2852f02780f83e22adc1e02b4
Epicsociety <measkswho@gmail.com>
2023-07-27 15:49:44 +0300

    a function that return an array of 3 ClassRoom objects

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,10 @@
+import ClassRoom from './0-classroom';
+
+function initializeRooms() {
+  const rm1 = new ClassRoom(19);
+  const rm2 = new ClassRoom(20);
+  const rm3 = new ClassRoom(34);
+  return [rm1, rm2, rm3];
+}
+
+export default initializeRooms;
