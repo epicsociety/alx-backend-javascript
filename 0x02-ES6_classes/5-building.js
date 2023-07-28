a43c073144d679cffedbf122802009534f2bde25
Epicsociety <measkswho@gmail.com>
2023-07-28 09:26:21 +0300

    add  evacuationWarningMessage() method and require subclasses to implement it

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -3,7 +3,12 @@
 
 export default class Building {
   constructor(sqft) {
-    this.sqft = sqft;
+    // eslint-disable-next-line  no-prototype-builtins
+    if (!(Object.getPrototypeOf(this).hasOwnProperty('evacuationWarningMessage'))) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    } else {
+      this.sqft = sqft;
+    }
   }
 
   get sqft() {
@@ -18,22 +23,3 @@ export default class Building {
     }
   }
 }
-
-const BuildingProxy = new Proxy(Building, {
-    construct(target, args) {
-      const instance = new target(...args);
-      const classConstructor = new.target;
-  
-      if (instance instanceof classConstructor) {
-        // Check if the instance is not the Building class itself
-        // Throw an error if evacuationWarningMessage is not implemented
-        if (instance.constructor === Building && !('evacuationWarningMessage' in instance)) {
-          throw new Error('Class extending Building must override evacuationWarningMessage');
-        }
-      }
-  
-      return instance;
-    },
-  });
-  
-  export { BuildingProxy as Building };
