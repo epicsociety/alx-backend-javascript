bacd21c9b1a5c4ff5e849bb070cdc12e248d1344
Epicsociety <measkswho@gmail.com>
2023-07-28 03:22:49 +0300

    Override the method named evacuationWarningMessage and return a string

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-underscore-dangle */
+
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft); // Call the parent class constructor to set sqft
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors.`;
+  }
+}
