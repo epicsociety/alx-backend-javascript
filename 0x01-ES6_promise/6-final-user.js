4e0fa48c854646367d00c3855078a4b5beba52d7
Epicsociety <measkswho@gmail.com>
2023-07-29 04:58:19 +0300

    Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,19 @@
+// 6-final-user.js
+
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpPromise, uploadPhotoPromise]).then((results) => {
+    return results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return { status: 'fulfilled', value: result.value };
+      } else {
+        return { status: 'rejected', value: result.reason };
+      }
+    });
+  });
+}
