58889a75e1b511ac5ca9cce58cceb162b61b0107
Epicsociety <measkswho@gmail.com>
2023-08-02 02:25:34 +0300

    an updated map for all items with initial quantity at 1

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,15 +1,14 @@
 // 10-update_uniq_items.js
 function updateUniqueItems(inputMap) {
-    if (!(inputMap instanceof Map)) {
-      throw new Error("Cannot process");
-    }
-  
-    for (const [item, quantity] of inputMap) {
-      if (quantity === 1) {
-        inputMap.set(item, 100);
-      }
+  if (!(inputMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  for (const [item, quantity] of inputMap) {
+    if (quantity === 1) {
+      inputMap.set(item, 100);
     }
   }
-  
-  export default updateUniqueItems;
-  
+}
+
+export default updateUniqueItems;
