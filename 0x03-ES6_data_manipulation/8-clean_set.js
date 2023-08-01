c557a9c32e9aac14430e3d86180c56ca85df9e9f
Epicsociety <measkswho@gmail.com>
2023-08-01 19:23:37 +0300

    return a string of all the set values that start with a specific string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   const concatString = [];
   set.forEach((value) => {
-    if (startString.length > 0 && value.startsWith(startString)) {
+    if (startString !== '' && value.startsWith(startString)) {
       concatString.push(value.slice(startString.length));
     }
   });
