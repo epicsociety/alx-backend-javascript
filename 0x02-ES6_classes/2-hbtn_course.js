21f74a07943ff096b52f78a0e51732a2000bd127
Epicsociety <measkswho@gmail.com>
2023-09-29 02:34:24 +0300

    Added type check in the constructor

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-underscore-dangle */
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (name !== 'string') {
+    if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
-    if (length !== 'number') {
+    if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
     if (!Array.isArray(students) || students.every((item) => typeof item !== 'string')) {
-      throw new TypeError('Students must be an array of strings');
+      throw new TypeError('Students must be an array of strings')
     } else {
       this._name = name;
       this._length = length;
