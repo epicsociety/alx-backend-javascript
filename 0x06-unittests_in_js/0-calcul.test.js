6f5b39a4528081417fae5a8151480dd7eff7f045
Epicsociety <measkswho@gmail.com>
2023-11-23 19:34:00 +0300

    Basic test with Mocha and Node assertion library

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul.js');
+
+describe('calculateNumber', () => {
+  it('should return the sum of rounded numbers for integer inputs', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+
+  it('should return the sum of rounded numbers for one decimal place inputs', () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it('should return the sum of rounded numbers for two decimal places inputs', () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it('should return the sum of rounded numbers for non-integer inputs', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+});
---
+++ 0x06-unittests_in_js/README.md
@@ -0,0 +1,8 @@
+## Some Resources
+
+- [Mocha documentation](https://mochajs.org/)
+- [Chai](https://www.chaijs.com/api/)
+- [Sinon](https://sinonjs.org/releases/v17/)
+- [Express](https://expressjs.com/en/guide/routing.html)
+- [Request](https://www.npmjs.com/package/request?activeTab=versions)
+- [How to Test NodeJS Apps using Mocha, Chai and SinonJS](https://www.digitalocean.com/community/tutorials/how-to-test-nodejs-apps-using-mocha-chai-and-sinonjs)
