0d78086115c9340c8b21060882ed1b09b9c1847b
Epicsociety <measkswho@gmail.com>
2023-11-23 23:16:07 +0300

    Basic Integration testing

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,15 +1,11 @@
-// api.js
-
 const express = require('express');
 const app = express();
 const port = 7865;
 
-app.listen(port, () => {
-  console.log(`API available on localhost port ${port}`);
-});
-
 app.get('/', (req, res) => {
-  res.send('Welcome to the payment system');
+  res.end('Welcome to the payment system');
 });
 
-module.exports = app;
+app.listen(port, () => {
+  console.log('API available on localhost port 7865');
+});
