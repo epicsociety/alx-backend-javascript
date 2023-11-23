2d50c8f996175c324e04d0a6e5d559ab3d59408c
Epicsociety <measkswho@gmail.com>
2023-11-23 19:59:53 +0300

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -17,4 +17,10 @@ describe('calculateNumber', () => {
   it('should return the sum of rounded numbers for non-integer inputs', () => {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
+  it('should return the sum of negative inputs', () => {
+    assert.strictEqual(calculateNumber(-1.5, -3.7), -5)
+  });
+  it('should return the sum of rounded 2 decimal inputs', () => {
+    assert.strictEqual(calculateNumber(1.57, 3.72), 6)
+  });
 });
